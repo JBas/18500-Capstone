cc195c35a7183f01309381fb32272c73994517b6
JBas <joshua.n.bas@gmail.com>
2020-03-06 16:11:16 -0500

    added some threading stuff

--- Software/core/core.py
+++ Software/core/core.py
@@ -9,65 +9,25 @@ error detection system.
 from __future__ import absolute_import, unicode_literals
 
 import octoprint.plugin
-import threading
-#import lib
-"""
-MAX_ERROR_CHECK = 5
-class CheckThread(threading.Thread):
-    def __init__(self, rpc, layer, group=None, target=None, name="check",
-                 args=(), kwargs=None, verbose=None):
-        self.cycles = 0
-        self.rpc = rpc
-        self.layer = layer
-        threading.Thread.__init__(self, group=group, tagert=target,
-                                  name=name, args=args, kwargs=kwargs,
-                                  verbose=verbose)
-        pass
-
-    def incrCycle(self):
-        self.cycles += 1
-        pass
-
-    def run(self):
-        I1 = lib.getI1()
-        #I2 = lib.getI2()
-
-        #ppc = PPC(I1, I2)
-        #ppc.generate()
-        #ppc_result = ppc.analyze(self.rpc, self.layer)
-
-        #edge_result = _____()
-
-        #do something... 
-        pass
-"""
-
-def worker():
-    print("Worker")
-    return
-
+#from lib import CheckThread
 
 class CorePlugin(octoprint.plugin.StartupPlugin,
                  octoprint.plugin.ShutdownPlugin):
-
-#    tlist = []
-
     def __init__(self):
         self.name    = "System Core"
         self.descr   = "Interface between OctoPrint and the rest of the computer vision"
         self.author  = "Joshua Bas, jnbas@andrew.cmu.edu, joshua.n.bas@gmail.com"
         self.url     = "https://github.com/JBas/18500-Capstone"
         self.layer = 0
+        self.tlist = []
         pass
 
     def on_after_startup(self):
-        self.__logger.info("Started up!")
-        t = threading.Thread(target=worker)
-        t.start()
+        self._logger.info("Started up!")
         pass
 
     def on_shutdown(self):
-        self.__logger.info("Shutting down!")
+        self._logger.info("Shutting down!")
         pass
 """    
     def handle_gcode_sent(comm_instance,
@@ -78,7 +38,19 @@ class CorePlugin(octoprint.plugin.StartupPlugin,
                              *args,
                              **kwargs):
         if gcode and gcode is "G0":
-            self.__logger.info("Sent a Z-axis change command!")
+            self._logger.info("Sent a Z-axis change command!")
+            self.layer += 1
+            for i in range(len(self.tlist), -1):
+                t = self.tlist[i]
+                if (t.isAlive()):
+                    t.incrCycles()
+                else:
+                    # do something with t.hasError
+                    self.tlist.pop(i)
+            t = CheckThread()
+            t.start()
+            self.tlist.append(t)
+
 
         return
 """
--- Software/core/lib.py
+++ Software/core/lib.py
@@ -1,9 +1,55 @@
 
-
+import threading
 import picamera
 
 I1_FILEPATH = "I1.png"
 I2_FILEPATH = "I2.png"
+
+MAX_ERROR_CHECK = 5
+
+class CheckThread(threading.Thread):
+    def __init__(self, rpc, layer, group=None, target=None, name="check",
+                 args=(), kwargs=None, verbose=None):
+        self.cycles = 0
+        self.rpc = rpc
+        self.layer = layer
+        self.cycleLock = threading.Lock()
+        threading.Thread.__init__(self, group=group, tagert=target,
+                                  name=name, args=args, kwargs=kwargs,
+                                  verbose=verbose)
+        pass
+
+    def incrCycles(self):
+        self.cycleLock.acquire(blocking=1)
+        self.cycles += 1
+        self.cycleLock.release()
+        pass
+    
+    def activate(self, ppc_result, edge_result):
+        ppc_weighting = 0.0
+        edge_weighting = 1.0
+        return bool(ppc_result*ppc_weighting + edge_result*edge_weight)
+
+    def run(self):
+        hasError = False
+        error = None
+
+        I1 = getI1()
+        #I2 = getI2()
+
+        #ppc = PPC(I1, I2)
+        #ppc.generate()
+        #ppc_result = ppc.analyze(self.rpc, self.layer)
+
+        #edge_result = _____()
+
+        hasError = activate(ppc_result, edge_result)
+
+        self.cycleLock.acquire(blocking=1)
+        if (self.cycles > MAX_ERROR_CHECK and hasError):
+            error = "System did not meet requirements!"
+        self.cycleLock.release()
+        return
 
 def getI1():
    """ 
