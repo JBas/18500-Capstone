19da5cb6a8aaceefe6bee6b9613094c19b88fa97
JBas <joshua.n.bas@gmail.com>
2020-04-26 13:05:55 -0400

    added Hannah's new gcode_to_3d_model (will have to redo credits, but thats ok

--- OctoPrint-Octoplus/octoprint_OctoPlus/gcode_to_3d_model.py
+++ OctoPrint-Octoplus/octoprint_OctoPlus/gcode_to_3d_model.py
@@ -1,44 +1,44 @@
-"""OctoPlus GCODE to 3D Model
-
-@author     Hannah Preston
-Implemented the original gcode_to_3d_model and graph_it functions that
-operate on x, y, z arrays
-
-@author     Joshua Bas
-Changed to operate on one array of (x,y,z) coords
-
-This program implements the 3D viewer for the OctoPlus plugin 
-"""
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-import matplotlib.image as mpimg
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-import cv2 
+from mpl_toolkits.mplot3d import Axes3D
 
-def graph_it(xcoords, ycoords, zcoords):
-    # open and read the file
+def run(file, theta):
+    [x,y,z] = gcode_to_3d_model(file)
+
+    graph_it(x,y,z,theta)
     
+
+def graph_it(xcoords, ycoords, zcoords,theta):
+
+    # for scaling so it doesn't look wonky 
+    max_x = np.amax(xcoords)
+    max_y = np.amax(ycoords)
+    max_z = np.amax(zcoords)
+
+    max_max = np.amax([max_x, max_y,max_z])
+
+    # set up 3d plot space
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
+    ax.set_xlim(-max_max/2, max_max/2)
+    ax.set_ylim(-max_max/2, max_max/2)
+    ax.set_zlim(0, max_max)
+
+    ax.view_init(elev=0.,azim = theta)
+
+    # plot figure
     ax.plot(xcoords, ycoords, zcoords, color='b')
 
-    plt.show()
+    #save figure
+    fig.savefig('test.png', bbox_inches='tight')
 
-def graph_it(ref):
-    # open and read the file
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot(ref[:, 0], ref[:, 1], ref[:, 2], color='b')
     plt.show()
 
 def gcode_to_3d_model(gcode_file):
     # open and read the file
     g_file = open(gcode_file, 'r')
     g_code = g_file.readlines()
-    g_file.close()
 
     x = []
     y = []
@@ -55,10 +55,8 @@ def gcode_to_3d_model(gcode_file):
             z_loc = g.find("Z")
             # end indicator
             f = g.find("F")
-            # print("line: %s\n z at %d and f at %d", (g,z_loc,f))
 
             # get coordinates
-            
             tx = float(g[x_loc+1:y_loc-1])
             ty = float(g[y_loc+1:z_loc-1])
             tz = float(g[z_loc+1:f-1])
@@ -67,46 +65,9 @@ def gcode_to_3d_model(gcode_file):
             x.append(tx)
             y.append(ty)
             z.append(tz)
+            
+    g_file.close()
 
-    ref = np.asarray(list(zip(x, y, z)))
-    ref = ref[ref[:, 2].argsort()]
-    ref = ref[:-1] # remove the last point bc it is just to move extruder to home position
-
-    return ref
-
-if __name__=="__main__":
-    filename ="data/gcode" 
-    img = mpimg.imread("data/image_0.jpg")
-    ref = gcode_to_3d_model(filename)
-
-    t = 10
-    Rz = np.array([[np.cos(t), -np.sin(t), 0],
-                   [np.sin(t), np.cos(t), 0],
-                   [0, 0, 1]])
-    Ry = np.array([[np.cos(t), 0, np.sin(t)],
-                   [0, 1, 0],
-                   [-np.sin(t), np.cos(t), 0]])
-
-    proj = Ry@(Rz@ref.T)
-    graph_it(ref)
-
-    #rvec = np.array([0,0,0], np.float) # rotation vector
-    #tvec = np.array([0,0,0], np.float) # translation vector
-    #fx = fy = 1
-    #cx = cy = 0
-    #cameraMatrix = np.array([[fx,0,cx],[0,fy,cy],[0,0,1]], np.float)
-
-    #result = cv2.projectPoints(ref, rvec, tvec, cameraMatrix, None)
-    #print(result[0].shape)
-    #fig = plt.figure()
-    #ax = fig.add_subplot(111)
-
-    #plt.imshow(img)
-    #ax.plot(result[0][:, 0, 0], result[0][:, 0, 1], color='b')
-
-    #plt.show()
-
-    #for n in range(len(ref)):
-    #    print(src[n], '==>', result[0][n])
-
-
+    return (x,y,z)
+        
+    
